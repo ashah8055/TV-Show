7686105cfb34086e5e92845eb7eb63c425c16648
Amar <adshah8055@gmail.com>
2019-11-24 22:36:37 -0500

    Search module implemented

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SearchService } from './search.service';
+import { Subject, Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [SearchService]
+})
+export class AppComponent implements OnInit, OnDestroy {
+
+  results$: Observable<string>;
+  searchTerm$ = new Subject<string>();
+
+  constructor(private searchService: SearchService) { }
+
+  ngOnInit(): void {
+    this.searchService.search(this.searchTerm$)
+      .subscribe(results => {
+        let ob = [results]
+        this.results$ = results;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.searchTerm$.unsubscribe();
+  }
+
+  onSearchChange(value: string): void {
+    this.searchTerm$.next(value)
+
+  }
+}
